8f4644045c3a24325f38fe0dcafb9dc0c9289426
Link4Electronics <179417190+Link4Electronics@users.noreply.github.com>
2026-06-13 07:15:49 -0300

    remove upx
    lots updates
    
    remove file dependency
    attempt to fix lib4bin lib symlink bugs
    remove dynamic linker patch
    remove dependency to /bin/sh when using AppRun script
    add /etc to XDG_DATA_DIRS
    add additional certificate path
    bring back hook for magic.mgc only

--- .github/create_aio_sharun.sh
+++ .github/create_aio_sharun.sh
@@ -3,9 +3,9 @@ set -e
 
 ARCH="$(uname -m)"
 
-apk add bash file binutils patchelf findutils grep sed coreutils strace which wget tar gzip
+apk add bash binutils patchelf findutils grep sed coreutils strace which wget tar gzip
 
-BINS="bash patchelf strip strace find file grep sed awk md5sum \
+BINS="bash patchelf strip strace find grep sed awk md5sum \
 xargs kill rm cp ln mv sleep echo readlink chmod sort tr printenv \
 cut mkdir basename dirname uname wget tail date tar gzip ls"
 
